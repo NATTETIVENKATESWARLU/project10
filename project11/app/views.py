4ff3f346c4633103905991b0e3fd693ef15acf9d
NATTETIVENKATESWARLU <118797681+NATTETIVENKATESWARLU@users.noreply.github.com>
2022-12-03 19:40:47 +0530

    Add files via upload

---
+++ project11/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def venkey(request):
+    return render(request,'venkey.html')
+
+
+def ravi(request):
+    return render(request,'ravi.html')
